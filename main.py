fe5a45f678c089b1f85c35d5c2b2b725d651e6e7
FLAMING-AI <108722900+FLAMING-AI@users.noreply.github.com>
2022-07-16 22:08:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,30 +6,39 @@ import os
 import re
 import asyncio
 from datetime import datetime
-
+from config import *
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-API_ID = 6435225
-API_HASH = "4e984ea35f854762dcde906dce426c2d" 
-LOG_GROUP = os.environ.get("LOG_GROUP", None)
-ALIVE_IMG = os.environ.get("ALIVE_PIC", None)
-STRING_SESSION1 = os.environ.get("STRING_SESSION1", None)
+API_ID = API_ID
+API_HASH = API_HASH 
+LOG_GROUP = LOG_GROUP
+SUDO_USERS = SUDO_USERS
+DB_URL = DB_URL
 
-SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "1829237132").split()}
-DB_URL = os.environ.get("DATABASE_URL", None)
-MONGO_DBB = os.environ.get("MONGO_DB", None)
+
+if not STRING_SESSION1:
+    logging.error("No String Session Found! Exiting!")
+    quit(1)
+
+if not API_ID:
+    logging.error("No Api-ID Found! Exiting!")
+    quit(1)
+
+if not API_HASH:
+    logging.error("No ApiHash Found! Exiting!")
+    quit(1)
 
 if ALIVE_IMG:
     ALIVE_PIC = ALIVE_IMG
 else: 
     ALIVE_PIC = 'https://te.legra.ph/file/c1672f2d5ac436575fc3b.mp4'
 
-if MONGO_DBB:
-    MONGO_DB = MONGO_DBB
+if MONGO_DB:
+    MONGO_DB = MONGO_DB
 else: 
     MONGO_DB = "https://visionbypankaj:Rf2BMcooABO6lP6K@cluster0.dgwig.mongodb.net/?retryWrites=true&w=majority"
 
@@ -38,27 +47,21 @@ if LOG_GROUP:
 else:
     Owner = 777000
 
+
 if STRING_SESSION1:
     bot1 = Client(session_name= STRING_SESSION1, api_id = API_ID, api_hash = API_HASH , plugins=dict(root="handlers"))
 else:
     bot1 = None
-
-
-
-
-
+    
 scheduler = AsyncIOScheduler()
 CMD_HELP = {}
 START_TIME = datetime.now()
 
 if bot1:
     bot1.start()
-    bot1.join_chat("flamingsupport")
     bot1.join_chat("flamingchat")
 
 idle()
 
-
 print("YPUR FLAMING BOT HAS BEEN DEPLOYED")
 print("Enjoy! Do visit @flamingsupport")
-
